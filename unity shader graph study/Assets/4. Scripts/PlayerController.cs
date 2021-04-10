4bce044f1b670a2393b79add23b660d8b50eeef8
tjwngur1731 <1sjhjs1@gmail.com>
2021-04-10 13:00:17 +0900

    First

---
+++ unity shader graph study/Assets/4. Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private bool isWalk;
+    [SerializeField]
+    private bool isRun;
+
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        anim.SetBool("isWalk", isWalk);
+        anim.SetBool("isRun", isRun);
+    }
+}
